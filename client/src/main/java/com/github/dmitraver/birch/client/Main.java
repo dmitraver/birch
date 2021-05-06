2fb1093b7fca7314464d0968ace8518bd142ec02
Dmitry Avershin <dmitraver@gmail.com>
2021-05-06 19:08:58 +0200

    Splits project into submodules - client and server.

---
+++ client/src/main/java/com/github/dmitraver/birch/client/Main.java
@@ -0,0 +1,7 @@
+package com.github.dmitraver.birch.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from client!");
+    }
+}
---
+++ server/src/main/java/com/github/dmitraver/birch/server/Main.java
@@ -0,0 +1,7 @@
+package com.github.dmitraver.birch.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from server!");
+    }
+}
