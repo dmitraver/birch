a82f71c7376e2bf57d9c326c64c58c236dd514ac
Dmitry Avershin <dmitraver@gmail.com>
2021-05-08 09:19:44 +0200

    Implements dummy single client server and client that can send requests to the server.

--- client/src/main/java/com/github/dmitraver/birch/client/Main.java
+++ client/src/main/java/com/github/dmitraver/birch/client/Main.java
@@ -1,7 +1,33 @@
 package com.github.dmitraver.birch.client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from client!");
+        String host = "localhost";
+        int port = 9999;
+
+        try(Socket socket = new Socket(host, port)) {
+            PrintWriter writer  = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            BufferedReader stdInReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String line;
+            while((line = stdInReader.readLine()) != null) {
+                writer.println(line);
+
+                String serverReply = reader.readLine();
+                System.out.println("Server: " + serverReply);
+
+                if (line.equals("QUIT")) {
+                    System.out.println("Disconnected...");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Opps, exception: " + e.getMessage());
+        }
     }
 }
--- server/src/main/java/com/github/dmitraver/birch/server/Main.java
+++ server/src/main/java/com/github/dmitraver/birch/server/Main.java
@@ -1,7 +1,38 @@
 package com.github.dmitraver.birch.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from server!");
+        int port = 9999;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                System.out.println("Waiting for the clients to connect...");
+                try (Socket clientSocket = serverSocket.accept()) {
+                    System.out.println("Client connected...");
+                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.equals("QUIT")) {
+                            writer.println("Cya soon...");
+                            break;
+                        } else {
+                            System.out.println("Client: " + line);
+                            writer.println("Got it!");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Opps, exception " + e.getMessage());
+        }
     }
 }
