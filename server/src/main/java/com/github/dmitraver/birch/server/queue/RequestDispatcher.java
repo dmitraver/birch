5056382f32ab7298e3df090b6c6f8eee9e8df631
Dmitry Avershin <dmitraver@gmail.com>
2021-05-23 08:26:35 +0200

    Better use of generic types.

--- server/src/main/java/com/github/dmitraver/birch/server/queue/RequestDispatcher.java
+++ server/src/main/java/com/github/dmitraver/birch/server/queue/RequestDispatcher.java
@@ -7,17 +7,17 @@ import com.github.dmitraver.birch.protocol.requests.WriteRequest;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-public final class RequestDispatcher {
+public final class RequestDispatcher<Response> {
 
-    private ReadRequestsQueue<ReadRequest, Optional<String>> readRequestsQueue;
-    private WriteRequestsQueue<WriteRequest, Optional<String>> writeRequestsQueue;
+    private ReadRequestsQueue<ReadRequest, Response> readRequestsQueue;
+    private WriteRequestsQueue<WriteRequest, Response> writeRequestsQueue;
 
-    public RequestDispatcher(ReadRequestsQueue<ReadRequest, Optional<String>> readRequestsQueue, WriteRequestsQueue<WriteRequest, Optional<String>> writeRequestsQueue) {
+    public RequestDispatcher(ReadRequestsQueue<ReadRequest, Response> readRequestsQueue, WriteRequestsQueue<WriteRequest, Response> writeRequestsQueue) {
         this.readRequestsQueue = readRequestsQueue;
         this.writeRequestsQueue = writeRequestsQueue;
     }
 
-    public Optional<CompletableFuture<Optional<String>>> dispatchRequest(Request request) {
+    public Optional<CompletableFuture<Response>> dispatchRequest(Request request) {
         if (request instanceof ReadRequest) {
             return Optional.of(readRequestsQueue.submit((ReadRequest) request));
         } else if (request instanceof WriteRequest) {
